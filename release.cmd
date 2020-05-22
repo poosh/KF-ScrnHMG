de3f70bc0b3d936184174455e7bbc4420bec072a
poosh <pooshrulez@gmail.com>
2020-05-22 16:18:45 +0300

    Enhanced make scripts

--- release.cmd
+++ release.cmd
@@ -1,58 +1,70 @@
 @echo off
 
-rem DON'T FORGET to Duplicate HumanPawn and PlayerController class code to the SZ_ScrnBalance!
-
 setlocal
-set KFDIR=d:\Games\kf
-set STEAMDIR=c:\Steam\steamapps\common\KillingFloor
-set outputdir=D:\KFOut\ScrnHMG
+
+set CURDIR=%~dp0
+call ..\ScrnMakeEnv.cmd %CURDIR%
 
 echo Removing previous release files...
-del /S /Q %outputdir%\*
+del /S /Q %RELEASEDIR%\*
 
+:: Sanity check
+if exist %RELEASEDIR%\System\%KFPACKAGE%.u (
+    echo Failed to cleanup the release directory
+    set /A ERR=100
+    goto :error
+)
+
+del %KFDIR%\System\%KFPACKAGE%.ucl 2>nul
 
 echo Compiling project...
 call make.cmd
-if %ERRORLEVEL% NEQ 0 goto end
+set /A ERR=%ERRORLEVEL%
+if %ERR% NEQ 0 goto error
 
 echo Exporting .int file...
-%KFDIR%\system\ucc dumpint ScrnHMG.u
+%KFDIR%\System\ucc dumpint %KFPACKAGE%.u
 
 echo.
 echo Copying release files...
-mkdir %outputdir%\Animations
-mkdir %outputdir%\Sounds
-mkdir %outputdir%\System
-mkdir %outputdir%\Textures
-mkdir %outputdir%\uz2
+xcopy /F /I /Y *.ini %RELEASEDIR%
+xcopy /F /I /Y *.txt %RELEASEDIR%
+xcopy /F /I /Y *.md  %RELEASEDIR%
 
+mkdir %RELEASEDIR%\System 2>nul
+xcopy /F /I /Y %KFDIR%\System\%KFPACKAGE%.int %RELEASEDIR%\System\
+xcopy /F /I /Y %KFDIR%\System\%KFPACKAGE%.u %RELEASEDIR%\System\
+xcopy /F /I /Y %KFDIR%\System\%KFPACKAGE%.ucl %RELEASEDIR%\System\
 
-copy /y %KFDIR%\system\ScrnHMG.* %outputdir%\System\
-copy /y %STEAMDIR%\Animations\HMG_A.ukx %outputdir%\Animations\
-copy /y %STEAMDIR%\Sounds\HMG_S.uax %outputdir%\Sounds\
-copy /y %STEAMDIR%\Textures\HMG_T.utx %outputdir%\Textures\
-copy /y %STEAMDIR%\Textures\BDFonts.utx %outputdir%\Textures\
-copy /y readme.txt  %outputdir%
-copy /y changes.txt  %outputdir%
+mkdir %RELEASEDIR%\Animations 2>nul
+xcopy /F /I /Y %STEAMDIR%\Animations\HMG_A.ukx %RELEASEDIR%\Animations\
 
+mkdir %RELEASEDIR%\Sounds 2>nul
+xcopy /F /I /Y %STEAMDIR%\Sounds\HMG_S.uax %RELEASEDIR%\Sounds\
 
-echo Compressing to .uz2...
-%KFDIR%\system\ucc compress %KFDIR%\system\ScrnHMG.u
-%KFDIR%\system\ucc compress %STEAMDIR%\Animations\HMG_A.ukx
-%KFDIR%\system\ucc compress %STEAMDIR%\Sounds\HMG_S.uax
-%KFDIR%\system\ucc compress %STEAMDIR%\Textures\HMG_T.utx
-%KFDIR%\system\ucc compress %STEAMDIR%\Textures\BDFonts.utx
+REM mkdir %RELEASEDIR%\StaticMeshes 2>nul
 
-move /y %KFDIR%\system\ScrnHMG.u.uz2 %outputdir%\uz2
-move /y %STEAMDIR%\Animations\HMG_A.ukx.uz2 %outputdir%\uz2
-move /y %STEAMDIR%\Sounds\HMG_S.uax.uz2 %outputdir%\uz2
-move /y %STEAMDIR%\Textures\HMG_T.utx.uz2 %outputdir%\uz2
-move /y %STEAMDIR%\Textures\BDFonts.utx.uz2 %outputdir%\uz2
+mkdir %RELEASEDIR%\Textures 2>nul
+xcopy /F /I /Y %STEAMDIR%\Textures\HMG_T.utx %RELEASEDIR%\Textures\
+xcopy /F /I /Y %STEAMDIR%\Textures\BDFonts.utx %RELEASEDIR%\Textures\
+
+if not exist %RELEASEDIR%\System\%KFPACKAGE%.u (
+    echo Release failed
+    set /A ERR=101
+    goto :error
+)
+
+echo.
+echo Updating the bundle...
+xcopy /F /I /Y %RELEASEDIR%\System\*                %BUNDLEDIR%\System\
 
 echo Release is ready!
 
-endlocal
+goto :end
 
-pause
+:error
+color 0C
 
 :end
+endlocal & SET _EC=%ERR%
+exit /b %_EC%
